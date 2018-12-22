f92420958f3b6cca5b30abb50ca8acb7be1bf27d
Justin <goodmood_lm@126.com>
2018-12-22 20:19:45 +0800

    增加中文注释

--- resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/ClosedState.java
+++ resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/ClosedState.java
@@ -23,7 +23,9 @@ import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 
 final class ClosedState extends CircuitBreakerState {
 
+    // 关闭状态的度量指标
     private final CircuitBreakerMetrics circuitBreakerMetrics;
+    // 请求调用的失败率阈值
     private final float failureRateThreshold;
 
     ClosedState(CircuitBreakerStateMachine stateMachine) {
@@ -43,8 +45,8 @@ final class ClosedState extends CircuitBreakerState {
     }
 
     /**
+     * 在关闭状态下，总是允许请求调用后端接口
      * Returns always true, because the CircuitBreaker is closed.
-     *
      * @return always true, because the CircuitBreaker is closed.
      */
     @Override
@@ -52,12 +54,21 @@ final class ClosedState extends CircuitBreakerState {
         return true;
     }
 
+    /**
+     * 调用度量指标circuitBreakerMetrics.onError()方法记录请求调用失败并返回失败百分比
+     * 调用checkFailureRate方法检查是否达到阈值
+     * @param throwable
+     */
     @Override
     void onError(Throwable throwable) {
         // CircuitBreakerMetrics is thread-safe
         checkFailureRate(circuitBreakerMetrics.onError());
     }
 
+    /**
+     * 调用度量指标circuitBreakerMetrics.onError()方法记录请求调用成功并返回失败百分比
+     * 调用checkFailureRate方法检查是否达到阈值
+     */
     @Override
     void onSuccess() {
         // CircuitBreakerMetrics is thread-safe
@@ -65,9 +76,9 @@ final class ClosedState extends CircuitBreakerState {
     }
 
     /**
+     * 如果失败率大于等于阈值，则触发状态机，从关闭状态转换到打开状态
      * Checks if the current failure rate is above the threshold.
      * If the failure rate is above the threshold, transitions the state machine to OPEN state.
-     *
      * @param currentFailureRate the current failure rate
      */
     private void checkFailureRate(float currentFailureRate) {
