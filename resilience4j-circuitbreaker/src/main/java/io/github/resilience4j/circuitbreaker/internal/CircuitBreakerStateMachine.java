9e52dcab6647fcf39c5d1e163f5f2ea5d93d072f
Liming <liming@etcp.cn>
2018-12-28 20:01:26 +0800

    增加中文注释

--- resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/CircuitBreakerStateMachine.java
+++ resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/CircuitBreakerStateMachine.java
@@ -53,8 +53,11 @@ public final class CircuitBreakerStateMachine implements CircuitBreaker {
     private static final Logger LOG = LoggerFactory.getLogger(CircuitBreakerStateMachine.class);
 
     private final String name;
+    // 用AtomicReference保证对CircuitBreakerState引用的原子性
     private final AtomicReference<CircuitBreakerState> stateReference;
+    // 熔断器配置
     private final CircuitBreakerConfig circuitBreakerConfig;
+    // 事件处理器
     private final CircuitBreakerEventProcessor eventProcessor;
 
     /**
@@ -64,9 +67,13 @@ public final class CircuitBreakerStateMachine implements CircuitBreaker {
      * @param circuitBreakerConfig The CircuitBreaker configuration.
      */
     public CircuitBreakerStateMachine(String name, CircuitBreakerConfig circuitBreakerConfig) {
+        // 设置熔断器名称
         this.name = name;
+        // 设置熔断器配置
         this.circuitBreakerConfig = circuitBreakerConfig;
+        // 将状态机初始化为关闭状态
         this.stateReference = new AtomicReference<>(new ClosedState(this));
+        // 初始化熔断事件处理器
         this.eventProcessor = new CircuitBreakerEventProcessor();
     }
 
@@ -175,39 +182,62 @@ public final class CircuitBreakerStateMachine implements CircuitBreaker {
         publishResetEvent();
     }
 
+    /**
+     * 状态转换公共逻辑方法
+     * @param newState
+     * @param newStateGenerator
+     */
     private void stateTransition(State newState, Function<CircuitBreakerState, CircuitBreakerState> newStateGenerator) {
+        // 先获取当前状态，然后执行状态更新方法。由AtomicReference保证操作的原子性。
         CircuitBreakerState previousState = stateReference.getAndUpdate(currentState -> {
+            // 新状态如果与当前状态相等，则返回当前状态。采用枚举判等
             if (currentState.getState() == newState) {
                 return currentState;
             }
+            // 执行更新方法，stateReference持有新状态的引用
             return newStateGenerator.apply(currentState);
         });
+        // 如果当前状态与新状态不等，则发布状态转换事件
         if (previousState.getState() != newState) {
             publishStateTransitionEvent(StateTransition.transitionBetween(previousState.getState(), newState));
         }
     }
 
-
+    /**
+     * 转换到不可用状态
+     */
     @Override
     public void transitionToDisabledState() {
         stateTransition(DISABLED, currentState -> new DisabledState(this));
     }
 
+    /**
+     * 转换到强制打开状态
+     */
     @Override
     public void transitionToForcedOpenState() {
         stateTransition(FORCED_OPEN, currentState -> new ForcedOpenState(this));
     }
 
+    /**
+     * 转换到关闭状态
+     */
     @Override
     public void transitionToClosedState() {
         stateTransition(CLOSED, currentState -> new ClosedState(this, currentState.getMetrics()));
     }
 
+    /**
+     * 转换到打开状态
+     */
     @Override
     public void transitionToOpenState() {
         stateTransition(OPEN, currentState -> new OpenState(this, currentState.getMetrics()));
     }
 
+    /**
+     * 转换到半开状态
+     */
     @Override
     public void transitionToHalfOpenState() {
         stateTransition(HALF_OPEN, currentState -> new HalfOpenState(this));
@@ -274,6 +304,7 @@ public final class CircuitBreakerStateMachine implements CircuitBreaker {
     private class CircuitBreakerEventProcessor extends EventProcessor<CircuitBreakerEvent> implements EventConsumer<CircuitBreakerEvent>, EventPublisher {
         @Override
         public EventPublisher onSuccess(EventConsumer<CircuitBreakerOnSuccessEvent> onSuccessEventConsumer) {
+            // 注册用于处理请求调用成功事件的consumer
             registerConsumer(CircuitBreakerOnSuccessEvent.class, onSuccessEventConsumer);
             return this;
         }
@@ -308,6 +339,7 @@ public final class CircuitBreakerStateMachine implements CircuitBreaker {
             return this;
         }
 
+        /** 调用父类EventProcessor.processEvent方法处理事件 **/
         @Override
         public void consumeEvent(CircuitBreakerEvent event) {
             super.processEvent(event);
